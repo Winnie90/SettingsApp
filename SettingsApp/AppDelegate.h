4ec53b52c8ae5f9e58fb02a8641844883bd243ab
Chris Winstanley <c.winstanley90@gmail.com>
2017-01-10 18:45:50 +0000

    Initial Commit

---
+++ SettingsApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SettingsApp
+//
+//  Created by Christopher Winstanley on 10/01/2017.
+//  Copyright © 2017 Winstanley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SettingsApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SettingsApp
+//
+//  Created by Christopher Winstanley on 10/01/2017.
+//  Copyright © 2017 Winstanley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
